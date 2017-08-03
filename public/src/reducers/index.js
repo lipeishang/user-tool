40ea72a5502f94fb26e6e64f659bca2ad10eca5f
lipeishang <1315789454@qq.com>
2017-08-03 13:57:31 +0800

    modify main.js

---
+++ public/src/reducers/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by lipeishang on 17-7-31.
+ */
+import {combineReducers} from 'redux';
+import ShowAllUsers from './ShowAllUsers';
+
+export default combineReducers({
+    ShowAllUsers
+});
