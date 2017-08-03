835ce869dfbf090723941fe39e65d7b6c31567a6
lipeishang <1315789454@qq.com>
2017-08-03 15:38:36 +0800

    add delete-spec

---
+++ spec/routers/delete-spec.js
@@ -0,0 +1,24 @@
+const app = require('../../app');
+const supertest = require('supertest');
+const expect = require('expect');
+import request from 'supertest';
+
+describe('delete /deleteUser', () => {
+    it('should return "status":true', (done)=> {
+        request(app)
+            .post('/deleteUser')
+            .send({"phonenumber":15829061748})
+            .end((err, res) => {
+                if (err) {
+                    done.fail(err);
+                }
+                else {
+                    let result = res.body;
+                    let expectResult =
+                        {"status":true};
+                    expect(result).toEqual(expectResult);
+                    done();
+                }
+            })
+    })
+});
