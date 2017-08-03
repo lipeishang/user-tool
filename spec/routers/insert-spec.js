87a810cf8f134f462077199240b4710e2bba977d
lipeishang <1315789454@qq.com>
2017-08-03 22:14:46 +0800

    add insert spec

--- spec/routers/insert-spec.js
+++ spec/routers/insert-spec.js
@@ -1,3 +1,32 @@
 /**
  * Created by lipeishang on 17-8-3.
  */
+const app = require('../../app');
+const supertest = require('supertest');
+const expect = require('expect');
+import request from 'supertest';
+let db = require('../../server/tools/connection');
+
+describe('post /insert', () => {
+      var user;
+      beforeEach( ()=> {
+      user = {
+            userName:'李佩赏',
+            name:'李佩赏',
+            sex:'女',
+            age:23,
+            phonenumber:'15829061748',
+            email:'15829061748@163.com',
+            remark:'无'
+        }
+    });
+    afterEach(()=>{
+        db.query("SELECT * FROM userInfo WHERE phonenumber=?",user.phonenumber)
+    });
+    it('should insert user is true',(done) => {
+        request(app)
+            .post('/insertUser')
+            .send(user)
+            .expect(200,done);
+    })
+});
--- spec/routers/search-spec.js
+++ spec/routers/search-spec.js
@@ -1,3 +1,16 @@
 /**
  * Created by lipeishang on 17-8-3.
  */
+const app = require('../../app');
+const supertest = require('supertest');
+const expect = require('expect');
+import request from 'supertest';
+
+describe('test search user',()=>{
+    it('should search user is true',(done)=>{
+        request(server)
+            .post('/searchUser')
+            .send({userName:'ly'})
+            .expect(200,done)
+    })
+});
